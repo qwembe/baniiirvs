2a214f28d803e8d3057bcf5ffb8848cbc3263185
qwembe <qwembe@gmail.com>
2018-12-11 15:03:05 +0300

    all servers created
    добавлены все серверы соответсвуюшие процессам (см рисунок)
    каждый сервер экспресс настроен так, что все модули берет из корня проекта.
    порты:
    экипаж самолета: 3000 npm run plane
    диспетчер АДП:3001  npm run dadp
    дис-р руления(?): 3002 npm run drulen
    дис-р старта(? что он делает): 3003 npm run dstart
    д-р АДЦ(?): 3004 npm run dadc
    д-р РДЦ(?): 3005 npm run drdc
    Они действительно все нужны?

--- dispetcherStarta/routes/index.js
+++ dispetcherStarta/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Plane page' });
+  res.render('index', { title: 'dispetcherStarta page' });
 });
 
 module.exports = router;
