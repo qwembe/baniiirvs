8fad3d1edff2d9ffbf8b6a4e640e98167154edb9
qwembe <qwembe@gmail.com>
2018-12-15 23:08:17 +0300

    done to s5 "Launch Engie"
    промежуточный коммит

--- plane/public/javascripts/plane.js
+++ plane/public/javascripts/plane.js
@@ -15,61 +15,112 @@ runnway_entry.disabled = "disabled";
 liftoff.disabled = "disabled";
 
 function click_permit() {
-    request_permit.disabled = "disabled";
-    request_launch_engie.disabled = "";
+    permit.disabled = "disabled";
 }
 
 function click_launch_engie() {
-    request_launch_engie.disabled = "disabled";
+    launch_engie.disabled = "";
+    //request_launch_engie.disabled = "disabled";
 }
 
 function click_runnway_entry() {
-    request_runnway_entry.disabled = "disabled";
+    runnway_entry.disabled = "";
 }
 
 function click_liftoff() {
-    request_liftoff.disabled = "disabled";
+    liftoff.disabled = "";
 }
 
 
-socket.on("hello", (data) => {
-    console.log(data)
-    if (data.type === "message") {
-        out.innerHTML += `<div> ${data.data}</div>`
-        current_state = data.data;
-    }
-})
-
-function message(){
-    
-}
-
-socket.on("currentState", (data) => {
-    console.log(data)
-    if (data.type === "updateState") {
-        state.innerHTML = `<div> ${data.data}</div>`
-        current_state = data.data;
-    }
-})
-
-request_permit.onclick= () => {
-    alert("!")
+request_permit.onclick = () => {
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) callback(this.responseText);
     };
     xhttp.open("POST", "http://localhost:3000/ask_permit", true);
     xhttp.send();
-
     click_permit();
 }
-request_launch_engie.onclick(() => {
+request_launch_engie.onclick = () => {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) callback(this.responseText);
+    };
+    xhttp.open("POST", "http://localhost:3000/ask_launch", true);
+    xhttp.send();
+    launch_engie.disabled = "disabled"
 
-})
-request_runnway_entry.onclick(() => {
+}
+request_runnway_entry.onclick = () => {
 
-})
-request_liftoff.onclick(() => {
+}
+request_liftoff.onclick = () => {
 
+}
+
+function message(msg){
+    out.innerHTML += `<div>${new Date()} : ${msg}</div>`
+}
+
+socket.on("currentState", (data) => {
+    // console.log(data)
+    if (data.type === "updateState") {
+        state.innerHTML = `<div> ${data.data}</div>`
+        current_state = data.data;
+    }
 })
+
+
+socket.on("ask_permit", (data) => {
+      console.log(data)
+    if (data.type === "toADP") {
+        data = data.data;
+        const xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function (data) {
+            if (this.readyState == 4 && this.status == 200) {
+                processADP_Answer(data);
+
+            };
+        };
+        url = `http://${data.url}:${data.port}/ask_permit`
+        console.log(url)
+        xhttp.open("GET", url, true); //TODO ask adp?
+        // xhttp.send();
+        processADP_Answer(data); //TODO delete later
+    }
+})
+
+function processADP_Answer(data){
+    /*id(data.type === "")*/
+    console.log("answerADP")
+    socket.json.emit('answerADP',{type: "answerADP", data: true}) //TODO later data
+    click_launch_engie()
+}
+
+socket.on("ask_launch", (data) => {
+      console.log(data)
+    if (data.type === "toRulenia") {
+        data = data.data;
+        const xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function (data) {
+            if (this.readyState == 4 && this.status == 200) {
+                processRulenia_Answer(data);
+
+            };
+        };
+        url = `http://${data.url}:${data.port}/ask_permit`
+        console.log(url)
+        xhttp.open("GET", url, true); //TODO ask rulenia?
+        // xhttp.send();
+        processRulenia_Answer(data); //TODO delete later
+    }
+})
+
+function processRulenia_Answer(data){
+    /*id(data.type === "")*/
+    console.log("answerRulenia")
+    socket.json.emit('answerRulenia',{type: "answerRulenia", data: true}) //TODO later data
+    click_runnway_entry()
+}
+
 
--- plane/routes/index.js
+++ plane/routes/index.js
@@ -1,22 +1,50 @@
 var express = require('express');
 var router = express.Router();
 const io = require('socket.io').listen("3030");
-
 const state = require('../states').state;
+const adp = require("../../server_addresses").dispetcherADP
+const rulenia = require("../../server_addresses").dispetcherRulenia
+
 var current_state = state.WAIT;
+var BUTTON_LAUNCH_ENGIE_PRESSED = false
 
 io.sockets.on('connection', (socket) => {
     socket.json.emit('hello', {type: "message", data: current_state})
+
+    socket.on('answerADP',(data)=>{
+        if(data.type === "answerADP"){
+            if(data.data){
+                current_state = state.HAVE_PERMIT;
+            }
+        }
+    })
+
+    socket.on('answerRulenia',(data)=>{
+        console.log(data);
+        if(data.type === "answerRulenia"){
+            if(data.data){
+                current_state = state.LAUNCH_ENGIE;
+            }
+        }
+    })
 })
 
-function currenState(){
-    console.log(current_state);
+
+
+function currenState() {
+    console.log(current_state)
     io.sockets.json.emit('currentState', {type: "updateState", data: current_state})
 }
 
-io.sockets.on('ask_permit',() => {
-    socket.json.emit('ask_permit')
-})
+function askPermit() {
+    io.sockets.json.emit('ask_permit', {type: "toADP", data: adp})
+}
+
+function askLaunch(){
+    io.sockets.json.emit('ask_launch', {type: "toRulenia", data: rulenia})
+
+}
+
 // State machine !
 
 function stateMachine() {
@@ -26,9 +54,12 @@ function stateMachine() {
 
             break;
         case state.DONT_HAVE_PERMIT:
-
+            askPermit();
             break;
         case state.HAVE_PERMIT:
+            if(BUTTON_LAUNCH_ENGIE_PRESSED){
+                askLaunch()
+            }
 
             break;
         case state.LAUNCH_ENGIE:
@@ -65,6 +96,10 @@ router.get('/', function (req, res, next) {
 
 router.post('/ask_permit', function (req, res, next) {
     current_state = state.DONT_HAVE_PERMIT;
+});
+
+router.post('/ask_launch', function (req, res, next) {
+    BUTTON_LAUNCH_ENGIE_PRESSED = true;
 });
 
 
